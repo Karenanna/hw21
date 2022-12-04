58a62387d067d8c5b2a0ca416141650a21a1eca1
karen gevorgyan <karen-gev.22@mail.ru>
2022-12-05 01:53:55 +0300

    Сделал ДЗ 21

---
+++ src/Bus.java
@@ -0,0 +1,54 @@
+public class Bus extends Transport implements Competing {
+
+    private Capacity capacity;
+
+
+    public Bus(String brand, String model, double engineVolume, Capacity capacity) {
+        super(brand, model, engineVolume);
+        this.capacity = capacity;
+    }
+
+    public Capacity getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Capacity capacity) {
+        this.capacity = capacity;
+    }
+
+
+    @Override
+    public void startMove() {
+        System.out.println("Автобус поехала");
+    }
+
+    @Override
+    public void stopMove() {
+        System.out.println("Автобус остоновилась");
+    }
+
+    @Override
+    public void printType() {
+        if (capacity == null) {
+            System.out.println("Данных не недостатоно");
+        } else {
+            System.out.println(capacity.getFrom() + " " + capacity.getTo());
+
+            @Override
+            public void pitStop () {
+                System.out.println("Пит_стоп для Автобус ");
+            }
+
+            @Override
+            public void bestTime () {
+                System.out.println("Лучший круг Автобус ");
+            }
+
+            @Override
+            public void maxSpeed () {
+                System.out.println("Лучшее время для Автобус ");
+            }
+        }
+    }
+}
+
---
+++ src/Truck.java
@@ -0,0 +1,56 @@
+public class Truck  extends Transport implements Competing {
+
+    private Weight weight;
+
+
+    public Truck(String brand, String model, double engineVolume, Weight weight) {
+        super(brand, model, engineVolume);
+        this.weight = weight;
+
+    }
+
+    public Weight getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Weight weight) {
+        this.weight = weight;
+    }
+
+
+    @Override
+    public void startMove() {
+        System.out.println("Грузавая машина поехала");
+    }
+
+    @Override
+    public void stopMove() {
+        System.out.println("Грузавая машина остоновилась");
+    }
+
+    @Override
+    public void printType() {
+        if (weight == null) {
+            System.out.println("Данных не недостатоно");
+        } else {
+            System.out.println(weight.getFrom());
+
+            @Override
+            public void pitStop () {
+                System.out.println("Пит_стоп для грузавой машины");
+            }
+
+            @Override
+            public void bestTime () {
+                System.out.println("Лучший круг грузавой  машины");
+            }
+
+            @Override
+            public void maxSpeed () {
+                System.out.println("Лучшее время для грузавой  машины");
+            }
+        }
+    }
+}
+
+
